2b97a99bf49e2152194e104a4891349d2688174e
Vanessa Sifuentes C <50958793+Vanessa-SC@users.noreply.github.com>
2021-02-01 20:57:00 -0600

    header fecha y periodoActual

--- php/fecha.php
+++ php/fecha.php
@@ -1,5 +1,7 @@
 <?php
 
+
+header('Access-Control-Allow-Origin: *');
 /* Retorna la fecha actual */
 
 // Establecimiento de zona horaria
--- php/periodoActual.php
+++ php/periodoActual.php
@@ -1,4 +1,5 @@
 <?php 
+header('Access-Control-Allow-Origin: *');
 
 /*  Calcular el periodo actual  */
 
